5ca6f0785640758a7a46198108d806e3ebbfabfc
SteelManITA <concetto.pollicina@gmail.com>
2017-04-08 17:30:52 +0200

    primo commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+class Moneta
+{
+	private $score;
+
+	function lancia() {
+		return rand(0,1) == 1;
+	}
+
+	function getPunteggio($tiri) {
+		$this->score = $this->score - (($tiri-2) * 10);
+		if ($this->score <= 0) {
+			return "hai perso";
+		} else {
+			return "Hai totalizzato " . $this->score . " punti";
+		}
+	}
+
+	function gioca($faccia) {
+		$consecutivi = 0;
+		$count = 0;
+		do {
+			$consecutivi = ($faccia === $this->lancia()) ? ++$consecutivi : $consecutivi=0;
+			++$count;
+		} while ($consecutivi < 2);
+
+		return $this->getPunteggio($count);
+	}
+
+	function __construct() {
+		$this->score = 100;
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Palindroma</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+
+<body>
+	<div class="container" style="margin-top: 20px;">
+		<div class="row">
+			<div class="offset-md-2 col-md-8">
+				<form class="form-inline" method="POST" action="index.php">
+					
+					<select name="faccia" class="custom-select">
+						<option value="-1" selected>Scegli una faccia</option>
+						<option value="1">Testa</option>
+						<option value="0">Croce</option>
+					</select>
+
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<?php 
+	if (isset($_POST['faccia'])) {
+		if ($_POST['faccia'] != -1) {
+			$moneta = new Moneta();
+			echo $moneta->gioca(false);
+		}
+	}
+	?>
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+
+</html>
